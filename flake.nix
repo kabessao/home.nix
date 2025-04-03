902ea3fa399aab23a95ac5ad95d74c0ac32ae325
CyberDruga <soares.henrique495@gmail.com>
2025-04-02 23:38:16 -0300

    fixing zen-browser flake input

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
 
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "unstable";
     };
 
     neovim-config = {
