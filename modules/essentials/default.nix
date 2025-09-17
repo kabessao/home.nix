e089cbb3b1f2b1ad6cd3dfdbc420cc17c8e04ccb
CyberDruga <soares.henrique495@gmail.com>
2025-09-17 05:57:32 -0300

    Refactoring my essentials module
    * adding option to remove packages
    * moving stuff into the module

--- modules/essentials/default.nix
+++ modules/essentials/default.nix
@@ -1,19 +1,82 @@
-{pkgs, zen-browser, lib, config, unstable, ...}:
+{
+pkgs,
+zen-browser,
+lib,
+config,
+unstable,
+...
+}:
+
 let
+  utils = import "${pkgs.path}/nixos/lib/utils.nix" {
+    inherit config lib;
+    pkgs = null;
+  };
   self = config.myessentials;
-	cfg = config;
+	packages = with pkgs; [
+
+		bitwarden-desktop
+		lazygit
+		firefox
+		vesktop
+		ferdium
+		headsetcontrol
+		gnome-tweaks
+		chromium
+		gnome-extension-manager
+		xclip
+		vscode
+		jq
+		podman
+		unzip
+		flameshot
+		obs-studio
+		distrobox
+		ripgrep
+		steam-run
+		libnotify
+		openvpn
+		gum
+		onlyoffice-desktopeditors
+		easyeffects
+		dconf-editor
+		obsidian
+		pavucontrol
+		vlc
+		zen-browser.twilight 
+		unstable.ghostty
+		youtube-music
+
+		(writeShellScriptBin "nixwhere" ''
+			which $@ | xargs -I {} readlink -f {}
+		'')
+
+		];
 in
+
 {
 	options.myessentials = {
+
+		programs.direnv = {
+			enable = true;
+			enableBashIntegration = true;
+			nix-direnv.enable = true;
+		};
 
 		enable = lib.mkOption {
 			default = true;
 			type = lib.types.bool;
-			description = "Installed essential packages";
+			description = "Install essential packages";
 		};
 
-	};
+    excludePackages = lib.mkOption {
+      default = [ ];
+      example = lib.literalExpression "[ pkgs.vscode ]";
+      type = lib.types.listOf lib.types.package;
+      description = "Packages to remove from the list";
+    };
 
+	};
 
 	config = lib.mkIf self.enable {
 
@@ -22,44 +85,7 @@ in
 			enableBashIntegration = true;
 		}; 
 
-		home.packages = with pkgs; [
-
-			bitwarden-desktop
-			lazygit
-			firefox
-			vesktop
-			ferdium
-			headsetcontrol
-			gnome-tweaks
-			chromium
-			gnome-extension-manager
-			xclip
-			vscode
-			jq
-			podman
-			unzip
-			flameshot
-			obs-studio
-			distrobox
-			ripgrep
-			steam-run
-			libnotify
-			openvpn
-			gum
-			onlyoffice-desktopeditors
-			easyeffects
-			dconf-editor
-			obsidian
-			pavucontrol
-			vlc
-			zen-browser.twilight 
-			unstable.ghostty
-			youtube-music
-
-			(writeShellScriptBin "nixwhere" ''
-				which $@ | xargs -I {} readlink -f {}
-			'')
-
-		];
+		home.packages = utils.removePackagesByName packages self.excludePackages ; 
 	};
+
 }
