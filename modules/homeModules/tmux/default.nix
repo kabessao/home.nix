973fe77ac8123ad6deaee690cc6aad56820a2d56
CyberDruga <soares.henrique495@gmail.com>
2026-06-19 14:22:59 -0300

    Adding keybinds to tmux

--- modules/homeModules/tmux/default.nix
+++ modules/homeModules/tmux/default.nix
@@ -95,28 +95,30 @@
 
             set-window-option -g mode-keys vi
 
-            bind h split-window -h -c "#{pane_current_path}" 
-            bind v split-window -v -c "#{pane_current_path}" 
-            bind c new-window -c "#{pane_current_path}" 
-            bind-key -T copy-mode-vi v send-keys -X begin-selection
+            bind -N "Split window horizontally" h split-window -h -c "#{pane_current_path}" 
+            bind -N "Split window vertically" v split-window -v -c "#{pane_current_path}" 
+            bind -N "Create new window" c new-window -c "#{pane_current_path}" 
+            bind-key -N "Begin selection" -T copy-mode-vi v send-keys -X begin-selection 
+            bind-key -N "Begin block selection" -T copy-mode-vi C-v run-shell "tmux send-keys -X rectangle-on ; tmux send-keys -X begin-selection"
 
-            bind -r C-k select-pane -U
-            bind -r C-j select-pane -D
-            bind -r C-h select-pane -L
-            bind -r C-l select-pane -R
+            bind -N "Select the pane up" -r C-k select-pane -U 
+            bind -N "Select the pane down" -r C-j select-pane -D 
+            bind -N "Select the pane to the left" -r C-h select-pane -L 
+            bind -N "Select the pane to the right" -r C-l select-pane -R 
 
-            bind -r C-Space next-layout
-            bind -r C-n next-window
-            bind -r C-p previous-window
+            bind -N "Next layout" -r C-Space next-layout 
+            bind -N "Goto next window" -r C-n next-window 
+            bind -N "Goto previous window" -r C-p previous-window 
 
             # bind -T copy-mode-vi Enter send-keys -X copy-pipe-and-cancel "xclip -i -f -selection primary | xclip -i -selection clipboard"
-            bind -T copy-mode-vi y send-keys -X copy-pipe-and-cancel "xclip -i -f -selection primary | xclip -i -selection clipboard"
-            bind -T copy-mode-vi Escape send-keys -X cancel
+            bind -N "Copy text" -T copy-mode-vi y send-keys -X copy-pipe-and-cancel "xclip -i -f -selection primary | xclip -i -selection clipboard" 
+            bind -N "Exit from selection mode" -T copy-mode-vi Escape send-keys -X cancel 
+            bind -N "Clear Selection" -T copy-mode-vi Space run-shell "tmux send-keys -X clear-selection ; tmux send-keys -X rectangle-off"
 
-            bind -T copy-mode MouseDragEnd1Pane send -X copy-selection-no-clear 'pbcopy'
+            bind -N "Copy with mouse" -T copy-mode MouseDragEnd1Pane send -X copy-selection-no-clear 'pbcopy' 
             unbind -T copy-mode-vi MouseDragEnd1Pane 
 
-
+            # Plugins
             set -g @plugin 'jimeh/tmux-themepack'
             set -g @plugin 'tmux-plugins/tpm'
             set -g @plugin 'tmux-plugins/tmux-sensible'
